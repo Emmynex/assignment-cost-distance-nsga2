7b9f023732a61e9c11b77fc06722f26315ca45ef
Agbenin Emmanuel <agbeninemmanuel@gmail.com>
2026-01-09 13:40:27 -0600

    Feat- swap mutation class implemented

--- Permutation__Sampling.py
+++ Permutation__Sampling.py
@@ -7,4 +7,15 @@ class Permutation_Sampling(Sampling):
         return np.array([np.random.permutation(problem.n_var) for _ in range(n_samples)])
 
 
+class SwapMutation(Mutation):
+    def __init__(self, prob=0.2):
+        super().__init__()
+        self.prob = prob
 
+    def _do(self, problem, X, **kwargs):
+        X = X.copy()  # Important: work on a copy
+        for k in range(X.shape[0]):
+            if np.random.rand() < self.prob:
+                i, j = np.random.choice(X.shape[1], 2, replace=False)
+                X[k, i], X[k, j] = X[k, j], X[k, i]
+        return X
