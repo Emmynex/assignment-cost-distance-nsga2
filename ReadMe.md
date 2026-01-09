d220d07d6b0baed26a62374901a8ee6f6a194c2d
Agbenin Emmanuel <agbeninemmanuel@gmail.com>
2026-01-09 13:18:00 -0600

    Feat- Method to load data implemented

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+project/
+│
+├── ReadText.py              # Data loading utilities
+├── Permutation_Sampling.py  # Initial population generation
+├── SwapMutationOperator.py  # Mutation operator
+├── CrossOverOperator.py     # PMX crossover operator
+├── Optimizer.py             # Problem definition and algorithm setup
+└── Main.py                  # Execution script and visualization
---
+++ ReadText.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def load_data(path, n=100):
+    # method to load data into the list.
+    rows = []
+    current_row = []
+
+    with open(path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+
+            if line.isdigit() and int(line) == n and len(rows) == 0:
+                continue
+
+            #
+            nums = list(map(int, line.split()))
+            current_row.extend(nums)
+
+            if len(current_row) == n:
+                rows.append(current_row)
+                current_row = []
+
+    return rows
